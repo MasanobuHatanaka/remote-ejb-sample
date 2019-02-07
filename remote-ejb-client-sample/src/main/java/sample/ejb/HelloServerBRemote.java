d3a227664b535236f47cfec3ece47bac3c5a6af4
Masanobu Hatanaka <mhatanak@redhat.com>
2019-02-07 15:07:09 +0900

    Update HelloServerBRemote.java

--- remote-ejb-client-sample/src/main/java/sample/ejb/HelloServerBRemote.java
+++ remote-ejb-client-sample/src/main/java/sample/ejb/HelloServerBRemote.java
@@ -2,7 +2,11 @@ package sample.ejb;
 
 import javax.ejb.Remote;
 
+import org.jboss.ejb.client.annotation.ClientTransaction;
+import org.jboss.ejb.client.annotation.ClientTransactionPolicy;
+
 @Remote
+@ClientTransaction(ClientTransactionPolicy.NOT_SUPPORTED)
 public interface HelloServerBRemote {
 	String helloServerB();
 }
